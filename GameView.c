b697a103e71eabcc270405b29a73835149f024db
Carl liu <carlliu2012@gmail.com>
2014-09-26 10:29:10 +1000

    senario for DOUBLE_BACK to HIDE at CASTLE_DRACULA

--- GameView.c
+++ GameView.c
@@ -192,6 +192,10 @@ void Dracula(GameView gv, qlist q, char *play)
         push(q, 102 + (play[2] - '0'));
         if (curloc == TELEPORT){
             gv->healthPoints[PLAYER_DRACULA] += LIFE_GAIN_CASTLE_DRACULA;
+        }else if (curloc == HIDE &&
+			((curloc = (play[2] - '0' == 1) ? peek(q, 2) : peek(q, 1)) == TELEPORT ||
+			(curloc = (play[2] - '0' == 1) ? peek(q, 2) : peek(q, 1)) == CASTLE_DRACULA)){
+			gv->healthPoints[PLAYER_DRACULA] += LIFE_GAIN_CASTLE_DRACULA;
         }else if (curloc != CITY_UNKNOWN && curloc != HIDE && curloc != NOWHERE &&
             (curloc == SEA_UNKNOWN || idToType(curloc) == 2)){
             gv->healthPoints[PLAYER_DRACULA] -= LIFE_LOSS_SEA;
