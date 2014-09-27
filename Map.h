c4d80930989e1001469bdef4ef7478b3d0f3d402
Carl liu <carlliu2012@gmail.com>
2014-09-28 03:18:25 +1000

    Update Map.h

--- Map.h
+++ Map.h
@@ -20,5 +20,12 @@ void disposeMap(Map g);
 void showMap(Map g); 
 int  numV(Map g);
 int  numE(Map g, TransportID t);
+// Returns number of direct connections between two nodes 
+// TransportID type is a 3 element array 
+// type[0] is transport by ROAD
+// type[1] is transport by RAIL
+// type[2] is transport by BOAT
+int connections(Map g, LocationID start, TransportID type[]);
+LocationID *neighbor(Map g, LocationID start, int type[], int what, int len);
 
 #endif
