1266a69635f636b9a4e7a2a41e53e4e0eed5ee89
Carl liu <carlliu2012@gmail.com>
2014-09-28 03:17:55 +1000

    Update Map.c

--- Map.c
+++ Map.c
@@ -134,6 +134,32 @@ int numE(Map g, TransportID type)
     return nE;
 }
 
+// Returns number of direct connections between two nodes 
+int connections(Map g, LocationID start,int type[])
+{
+	VList curr = g->connections[start];
+	int i = 0;
+	for (; curr != NULL; curr = curr->next){
+		if (type[curr->type - 1]){
+			i++;
+		}
+	}
+	return i;
+}
+LocationID *neighbor(Map g, LocationID start, int type[], int what,int len)
+{
+	VList curr = g->connections[start];
+	int i = 0;
+	LocationID* Locortype = malloc(sizeof(LocationID)*len);
+	for (; curr != NULL; curr = curr->next){
+		if (type[curr->type - 1]){
+			if (what == 0)Locortype[i++] = curr->v;
+			else Locortype[i++] = curr->type;
+		}
+	}
+	return Locortype;
+}
+
 // Add edges to Graph representing map of Europe
 static void addConnections(Map g)
 {
