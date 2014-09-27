698faf5d60d89cc83b33b0d5c92c20d7c30389b4
Carl liu <carlliu2012@gmail.com>
2014-09-27 14:42:14 +1000

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,30 @@
+#ifndef H_QUEUE_H
+#define H_QUEUE_H
+
+#include "Places.h"
+#include "GameView.h"
+
+typedef struct queue *qlist;
+typedef struct trapvam *Trapvam;
+
+typedef struct node{
+    LocationID playerloc;
+    int trapNum;
+    struct node *next;
+    struct node *prev;
+}node;
+
+qlist init();
+int isNotEmpty(qlist q);
+LocationID spop(qlist q);
+LocationID pop(qlist q);
+LocationID peek(qlist q, int n);
+void push(qlist q, LocationID it);
+void pushtrap(qlist q, node *newt);
+void movetrap(qlist q, LocationID place);
+int trapNum(TrapVam tv, LocationID place, int trapType);
+void updateTrap(TrapVam tv,LocationID place,int type);
+void updateVam(TrapVam tv, LocationID place, int type);
+void freeTravm(TrapVam ts)
+
+#endif
