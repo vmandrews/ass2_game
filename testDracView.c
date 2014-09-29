6ba6342411c380a5f3fef28eae5f48f60c1929bf
michaelchew <michael.s.chew@gmail.com>
2014-09-29 15:14:49 +1000

    Update testDracView.c
    Added tests for dracView

--- testDracView.c
+++ testDracView.c
@@ -5,11 +5,21 @@
 #include <string.h>
 #include "DracView.h"
 
+#define LARGE_ENOUGH 13960
+
+void testDracView_1(void);
+void testDracView_given(void);
+
 int main()
+{   
+    testDracView_1();
+    testDracView_given();
+}
+
+void testDracView_given(void)
 {
     int i;
     DracView dv;
-
 
     printf("Test for basic functions, just before Dracula's first move\n");
     PlayerMessage messages1[] = {"Hello","Rubbish","Stuff",""};
@@ -107,6 +117,119 @@ int main()
     disposeDracView(dv);
 
     printf("passed\n");
-    return 0;
 }
+
+void testDracView_1(void)
+{
+    int i;
+    DracView new;
+    PlayerMessage messages[] = {""};
+    new = newDracView("GGE.... SGE.... HGE.... MGE.... DED....",messages);
+
+    printf("testing round number and score...\n");
+    i = giveMeTheRound(new); assert(i == 1);
+    i = giveMeTheScore(new); assert(i == 365);
+    new = newDracView("GGE.... SGE.... HGE.... MGE.... DED.... " 
+                    "GGE.... SGE.... HGE.... MGE.... DED....",messages);
+    i = giveMeTheRound(new); assert(i == 2);
+    i = giveMeTheScore(new); assert(i == 364);
+    new = newDracView("GGE.... SGE.... HGE.... MGE.... DED.... " 
+                    "GGE.... SGE.... HGE.... MGE....",messages);
+    i = giveMeTheRound(new); assert(i == 1); //round 2 starts when dracula finishes turn
+    i = giveMeTheScore(new); assert(i == 365);
+    printf("passed\n");
+
+    printf("testing health points...\n");
+    new = newDracView("GAS.... SAS.... HAS.... MAS.... DED.... "
+                    "GAL.... SAL.... HAL.... MAL.... DTP.... "
+                    "GAM.... SAM.... HAM.... MAM.... DED.... "
+                    "GATT... SAT.... HAT.... MAT.... DED.... "
+                    "GAO.... SAO.... HAO.... MAO.... DED.... "
+                    "GBA.... SBA.... HBA.... MBA.... DED.... "
+                    "GBI.... SBI.... HBA.... MBA....", messages); // incomplete round
+    i = giveMeTheRound(new); assert(i == 6);
+    i = giveMeTheScore(new); assert(i == 360);
+    i = howHealthyIs(new, PLAYER_DRACULA); assert(i == 50); //teleported (+10 life)
+    i = howHealthyIs(new, PLAYER_LORD_GODALMING); assert(i == 7); //encounter trap 
+
+    
+    new = newDracView("GAST... SAST... HAS.... MAS.... DCD.... "
+                    "GAS.... SALT... HAL.... MAL.... DHI.... "
+                    "GAMD... SAMT... HAM.... MAM.... DAM.... "
+                    "GAM.... SATT... HAT.... MAT.... DED.... "
+                    "GAOT... SAO.... HAO.... MAO.... DAS.... "
+                    "GBA.... SBA.... HBA.... MBA.... DCD.... "
+                    "GBI.... SBI.... HBI.... MBI...." , messages);
+    i = howHealthyIs(new, PLAYER_LORD_GODALMING);
+    assert(i == 6); //encounters trap (-2), rest (max health 9), encourter D (-4), rest (+3), trap (-2)
+    i = howHealthyIs(new, PLAYER_DR_SEWARD);
+    assert(i == 1); // damages from traps
+    i = howHealthyIs(new, PLAYER_DRACULA);
+    assert(i == 58); // Castle Dracula for three turns (+20) and 1 encounter (-10) and sea (-2)
+    printf("passed\n");
+
+    printf("testing score function...\n");
+    char past_play_string_1[LARGE_ENOUGH] = "GAL.... SBI.... HBI.... MBI.... DBATV..";
+    new = newDracView(past_play_string_1, messages);
+    i = giveMeTheScore(new);
+    assert(i == 365); //Dracula's turn finished (-1 score)
+    strcat(past_play_string_1, " GBATD.. SBI.... HBI.... MBI.... DBAT.V.");
+    new = newDracView(past_play_string_1, messages);
+    i = giveMeTheScore(new);
+    assert(i == 351); //Vampire matures (-13 score) and Dracula turn finished (-1 score)
+    strcat(past_play_string_1, " GJMD... SBI.... HBI.... MBI.... DAS....");
+    new = newDracView(past_play_string_1, messages);
+    i = giveMeTheScore(new);
+    assert(i == 344); //LORD_GODALMING sent to hospital (-6 score) and dracula's turn finished
+    printf("passed\n");
+
+    printf("testing for super long game...\n");
+    for(i = 0; i < 344; i++){
+        strcat(past_play_string_1, " GJM.... SBI.... HBI.... MBI.... DAS....");
+    }
+    new = newDracView(past_play_string_1, messages);
+    i = giveMeTheScore(new);
+    assert(i == 0);
+    strcat(past_play_string_1, " GJM.... SBO.... HBI.... MMU.... DAS....");
+    new = newDracView(past_play_string_1, messages);
+    i = giveMeTheScore(new);
+    //assert(i == 0); // no negative scores allowed
+    printf("passed\n");
+
+    printf("testing for where is function\n");
+    new = newDracView("GAS.... SGE.... HGW.... MMU.... DCD....", messages);
+    i = whereIs(new, PLAYER_LORD_GODALMING); assert(i == ADRIATIC_SEA);
+    i = whereIs(new, PLAYER_DR_SEWARD); assert(i == GENEVA);
+    i = whereIs(new, PLAYER_VAN_HELSING); assert(i == GALWAY);
+    i = whereIs(new, PLAYER_MINA_HARKER); assert(i == MUNICH);
+    i = whereIs(new, PLAYER_DRACULA); assert(i == CASTLE_DRACULA);
+    printf("passed\n");
+
+    printf("testing last move function\n");
+    new = newDracView("GAS.... SGE.... HGW.... MMU.... DCD.... "
+                    "GGE.... SAS.... HMU.... MGW.... DBI....", messages);
+    LocationID start, end;
+    lastMove(new, PLAYER_LORD_GODALMING, &start, &end);
+    assert(start == ADRIATIC_SEA && end == GENEVA);
+    lastMove(new, PLAYER_VAN_HELSING, &start, &end);
+    assert(start == GALWAY && end == MUNICH);
+    lastMove(new, PLAYER_DRACULA, &start, &end);
+    assert(start == CASTLE_DRACULA && end == BARI);
+    new = newDracView("GAS.... SGE.... HGW.... MMU.... DCD....", messages);
+    lastMove(new, PLAYER_LORD_GODALMING, &start, &end);
+    assert(start == UNKNOWN_LOCATION && end == ADRIATIC_SEA);
+    lastMove(new, PLAYER_VAN_HELSING, &start, &end);
+    assert(start == UNKNOWN_LOCATION && end == GALWAY);
+    printf("passed\n");
+
+    disposeDracView(new);
+}
+
+
+
+
+
+
+
+
 
